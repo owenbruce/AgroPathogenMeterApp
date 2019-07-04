49e038d3f7538c9d01a3ce8b59fe97ec98f0732d
Owen Bruce <owen.bruce@sympatico.ca>
2019-07-04 14:49:06 -0400

    Added btcontrol for ios

---
+++ AgroPathogenMeterApp/AgroPathogenMeterApp.iOS/BtControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text;
+using AgroPathogenMeterApp.iOS;
+using AgroPathogenMeterApp.Models;
+using Foundation;
+using UIKit;
+
+[assembly: Xamarin.Forms.Dependency(typeof(BtControl))]
+namespace AgroPathogenMeterApp.iOS
+{
+    class BtControl : IBtControl
+    {
+        public BtControl() { }
+        public static void Init() { }
+        public async Task<BtDatabase> TestConn()
+        {
+            BtDatabase _database = null;
+            return _database;
+        }
+        public async void Connect()
+        {
+
+        }
+        public string FilePath()
+        {
+            return "";
+        }
+    }
+}
